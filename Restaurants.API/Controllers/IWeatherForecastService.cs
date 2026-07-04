21061c330b427c63bef98587f56aae6da5584fb1
j.sanchez <jodsuedrk@gmail.com>
2026-07-04 11:57:38 -0300

    Primer Commit

---
+++ Restaurants.API/Controllers/IWeatherForecastService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Restaurants.API.Controllers
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get();
+    }
+}
---
+++ Restaurants.API/Controllers/WeatherForecastService.cs
@@ -0,0 +1,38 @@
+﻿namespace Restaurants.API.Controllers
+{
+    
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        /*
+        public IEnumerable<WeatherForecast> Get(int count, int minTemperature, int maxTemperature)
+        {
+            return Enumerable.Range(1, count).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(minTemperature, maxTemperature),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }*/
+
+        public IEnumerable<WeatherForecast> Get1(int count, int minTemperature, int maxTemperature)
+        {
+            return Enumerable.Range(1, count).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(minTemperature, maxTemperature),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+
+}
